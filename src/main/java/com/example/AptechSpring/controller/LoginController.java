ebae24780f5941ef8649cd38973a427ea78cc411
Dao Nguyen Minh Hoang <minhhoang905@gmail.com>
2024-12-12 12:51:00 +0700

    add login failed msg

--- src/main/java/com/example/AptechSpring/controller/LoginController.java
+++ src/main/java/com/example/AptechSpring/controller/LoginController.java
@@ -13,105 +13,109 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.AptechSpring.form.LoginForm;
 import com.example.AptechSpring.service.UserService;
 
-
-
-
 // Đánh dấu class này là 1 controller của spring
 @Controller
 public class LoginController {
 	@Autowired
-	private UserService userService; //Service chứa các phương thức
+	private UserService userService; // Service chứa các phương thức
 
-	
-    @SuppressWarnings("deprecation") // Loại bỏ cảnh báo deprecation, liên quan đến các phương thức hoặc lớp cũ
-	@GetMapping("/login")  //Dùng getmapping với authentication từ spring security để đăng nhập 
-    public String login(Authentication authentication) {
-    	//Kiểm tra xem người dùng đã đăng nhập hay chưa thông qua đối tượng Athentication
-        if (null != authentication){
-        	//Tạo biến String rồi gán tên đăng nhập của người dùng thông qua Authentication
-            String username = authentication.getName();
-            //Tạo if để kiểm tra xem tên đăng nhập của người dùng có bị rỗng không
-            if (!StringUtils.isEmpty(username)){
-            	//Nếu đã đăng nhập, điều hướng về link sau
-                return "redirect:/home/home1";
-            }
-        }
-      //Nếu chưa đăng nhập thì đưa về lại login
-    return "login";
-    }
-    
-//	//Tạo method để hiển thị page login.html
-//	@RequestMapping(path ="/login", method=RequestMethod.GET)
-//	public String login(ModelMap model) {
-//		return "login";
-//	}
-//	
-//	//Tạo method để nếu đúng username và mật khẩu thì vào trang home
-//	@RequestMapping(path ="/doLogin", method=RequestMethod.POST)
-//	public String doLogin(@ModelAttribute com.example.AptechSpring.form.LoginForm loginForm, ModelMap model) {
-//
-//		if (userService.IsValidateUser(loginForm.getUsername(), loginForm.getPassword())) {
-//		
-//			 // Thêm thuộc tính "message" vào mô hình với giá trị như sau
-//			model.addAttribute("mes", "LOGIN THANH CONG");
-//			return "redirect:/home/home1";
-//		}else {
-//			model.addAttribute("errMes", "Wrong name or password");
-//			return "login";
-//		}
-//	}
-	//Tạo method để hiển thị page register.html
-	@RequestMapping(path ="/register", method=RequestMethod.GET)
+	@SuppressWarnings("deprecation") // Loại bỏ cảnh báo deprecation, liên quan đến các phương thức hoặc lớp cũ
+	@GetMapping("/login") // Dùng getmapping với authentication từ spring security để đăng nhập
+	public String login(Authentication authentication, ModelMap model) {
+		// Kiểm tra xem người dùng đã đăng nhập hay chưa thông qua đối tượng
+		// Athentication
+		if (null != authentication) {
+			// Tạo biến String rồi gán tên đăng nhập của người dùng thông qua Authentication
+			String username = authentication.getName();
+			// Tạo if để kiểm tra xem tên đăng nhập của người dùng có bị rỗng không
+			if (!StringUtils.isEmpty(username)) {
+				// Nếu đã đăng nhập, điều hướng về link sau
+				return "redirect:/home/home1";
+			}
+		} else {
+			model.addAttribute("errMes", "Wrong name or password");
+		}
+		// Nếu chưa đăng nhập thì đưa về lại login
+		return "login";
+	}
+
+	// //Tạo method để hiển thị page login.html
+	// @RequestMapping(path ="/login", method=RequestMethod.GET)
+	// public String login(ModelMap model) {
+	// return "login";
+	// }
+	//
+	// //Tạo method để nếu đúng username và mật khẩu thì vào trang home
+	// @RequestMapping(path ="/doLogin", method=RequestMethod.POST)
+	// public String doLogin(@ModelAttribute com.example.AptechSpring.form.LoginForm
+	// loginForm, ModelMap model) {
+	//
+	// if (userService.IsValidateUser(loginForm.getUsername(),
+	// loginForm.getPassword())) {
+	//
+	// // Thêm thuộc tính "message" vào mô hình với giá trị như sau
+	// model.addAttribute("mes", "LOGIN THANH CONG");
+	// return "redirect:/home/home1";
+	// }else {
+	// model.addAttribute("errMes", "Wrong name or password");
+	// return "login";
+	// }
+	// }
+	// Tạo method để hiển thị page register.html
+	@RequestMapping(path = "/register", method = RequestMethod.GET)
 	public String register(ModelMap model) {
 		return "register";
 	}
-	
-	//Tạo method để đăng ký thông tin vào database
-	@RequestMapping(path = "/doRegister", method=RequestMethod.POST)
+
+	// Tạo method để đăng ký thông tin vào database
+	@RequestMapping(path = "/doRegister", method = RequestMethod.POST)
 	public String doRegister(@ModelAttribute LoginForm loginform, ModelMap model) {
-		
-		//Tạo biến boolean và dùng method IsValid() ở UserService để check điều kiện đăng ký
+
+		// Tạo biến boolean và dùng method IsValid() ở UserService để check điều kiện
+		// đăng ký
 		boolean isRegistered = userService.IsValid(loginform.getUsername(), loginform.getPassword(), model);
-		//Nếu thực hiện đăng ký thành công thì dùng method saveUser để lưu vào database
+		// Nếu thực hiện đăng ký thành công thì dùng method saveUser để lưu vào database
 		if (isRegistered) {
-			//Nếu đăng ký thành công, trả về trang login
+			// Nếu đăng ký thành công, trả về trang login
 			return "login";
 		} else {
-			//Nếu không thành công, trả về lại register
+			// Nếu không thành công, trả về lại register
 			return "register";
 		}
-	}	
+	}
 
-	//Tạo method để hiển thị changePass.html
+	// Tạo method để hiển thị changePass.html
 	@RequestMapping(path = "/changePass", method = RequestMethod.GET)
 	public String changePass(ModelMap model) {
 		return "changePass";
 	}
-	
-	//Tạo method để cập nhập mật khẩu
+
+	// Tạo method để cập nhập mật khẩu
 	@RequestMapping(path = "/doChangePass", method = RequestMethod.POST)
 	public String doChangePass(@ModelAttribute LoginForm loginForm, ModelMap model) {
-		
-		//Tạo 1 boolean và gọi method changePass từ UserService để kiểm tra và cập nhập mật khẩu
-		boolean isPasswordChanged = userService.changePass(	loginForm.getUsername(),
-															loginForm.getPassword(),
-															loginForm.getNewPassword());
-		//Tạo if-else để điều hướng trang web
+
+		// Tạo 1 boolean và gọi method changePass từ UserService để kiểm tra và cập nhập
+		// mật khẩu
+		boolean isPasswordChanged = userService.changePass(loginForm.getUsername(),
+				loginForm.getPassword(),
+				loginForm.getNewPassword());
+		// Tạo if-else để điều hướng trang web
 		if (isPasswordChanged) {
-			//Nếu thành công => Thêm thuộc tính "mes" để chuyển hướng tới trang login với thông báo
+			// Nếu thành công => Thêm thuộc tính "mes" để chuyển hướng tới trang login với
+			// thông báo
 			model.addAttribute("okMess", "Password changed successfully!");
 			// Đi đến trang login.html
 			return "login";
-		}else {
-			//Nếu thất bại => Thêm thuộc tính "mes" ở trang đổi mật khẩu với thông báo
+		} else {
+			// Nếu thất bại => Thêm thuộc tính "mes" ở trang đổi mật khẩu với thông báo
 			model.addAttribute("mes", "Invalid username or password");
-			//Quay trở lại trang ChangePass.html
+			// Quay trở lại trang ChangePass.html
 			return "changePass";
 		}
-	}	
-			
-	//Tạo method để hiển thị page login.html
-	@RequestMapping(path = "/logout",method = RequestMethod.GET)
+	}
+
+	// Tạo method để hiển thị page login.html
+	@RequestMapping(path = "/logout", method = RequestMethod.GET)
 	public String logout(ModelMap model) {
 		return "login";
 	}
